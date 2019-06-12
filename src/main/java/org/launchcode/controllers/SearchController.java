bbd5ea74ccdd0276fd95eca90b529bee205128db
Jacob Bellm <bellmbooks@gmail.com>
2019-06-11 23:11:38 -0500

    Added a search results handler

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -1,10 +1,11 @@
 package org.launchcode.controllers;
 
 import org.launchcode.models.JobData;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,6 +23,14 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+    /* #1 - Create handler to process search request and display results */
+    @RequestMapping(value = "/results", method = RequestMethod.GET)
+    public String results(@RequestParam String searchType, @RequestParam String searchTerm) {
+
+        ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        return "redirect";
+
+    
+    }
 
 }
